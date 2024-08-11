a5bc8fd3e1014c08d8ba58521b3fc387c2a0e1ef
Azu Chan <Kogaaz68@craighead.school.nz>
2024-08-11 21:57:12 +0000

    a

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 function playGame(){
  alert('Welcome to my rock, paper, scissors game')
  
- let knowGame = prompt('Do you know rock, paper, scissors game? Answer yes or no')
+ alert('Do you know rock, paper, scissors game? Answer yes or no')
+ let answer = prompt()
 
- if (knowGame() === 'no'){
-    alert('Rules description. There are three items - rock, scissors and paper - and everyone has to produce one of these items at the same time.The rock beats the scissors, the paper beats the rock and the scissors beat the paper.')
-    let userChoice = prompt("Enter your choice: rock, paper, or scissors")
-    let computerChoice = getComputerChoice();
-    let result = determineWinner(userChoice, computerChoice)
-   } 
+ if (answer == 'yes') {
+   alert("That's good! Let's play game!.")
+
+ }else{
+   alert('Rules description. There are three items - rock, scissors and paper - and everyone has to produce one of these items at the same time.The rock beats the scissors, the paper beats the rock and the scissors beat the paper.')
+   alert('Did you understand the rules of the game?')
+   let understand = prompt()
+   if(understand = 'yes'){
+   alert("Let's start game !")
+   }
+   }
+}
    function getComputerChoice(){
       const choice = ('rock,paper,scissors')
       const randam = ('')
-
+}
    function determineWinner(userChoice, computerChoice) {
 if (userChoice === computerChoice) {
     return "It's a tie!";
     }
 if (
-    (userChoice === "rock" && computerChoice === "scissors") ||
-    (userChoice === "scissors" && computerChoice === "paper") ||
+    (userChoice === "rock" && computerChoice === "scissors") 
+    (userChoice === "scissors" && computerChoice === "paper") 
     (userChoice === "paper" && computerChoice === "rock")
  ) {
    return "You win!";
@@ -27,5 +34,3 @@ if (
    return "You lose!";
    }
 }
-
-   
